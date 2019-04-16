df69bf99149e96b9254bda0c2700669b030cde52
PedroMiguelAlbuquerque <pedroalbuquerque1998@gmail.com>
2019-04-16 22:50:27 +0100

    refractoring

--- queries.sql
+++ queries.sql
@@ -1,26 +1,9 @@
-UPDATE TABLE edificio 
- SET coordenadas_gps = '66.56ºN 8.89ºO'
- WHERE id_edificio = 3;
- 
- UPDATE TABLE edificio 
- SET area = 150
- WHERE tipologia = 'loja';
- 
- UPDATE TABLE divisao 
- SET numero_janelas = 4
- WHERE id_edificio = 4;
- 
- UPDATE TABLE divisao 
- SET numero_janelas = 4
- WHERE id_edificio = 4;
- 
- UPDATE TABLE tarifa 
- SET preco_dia = 0,3
- WHERE id_tarifa = 1;
- 
- UPDATE TABLE tarifa 
- SET hora_mudanca_tarde = 13
- WHERE id_tarifa = 1;
+UPDATE TABLE edificio SET coordenadas_gps = '66.56ºN 8.89ºO' WHERE id_edificio = 3;
+UPDATE TABLE edificio SET area = 150 WHERE tipologia = 'loja';
+UPDATE TABLE divisao SET numero_janelas = 4 WHERE id_edificio = 4;
+UPDATE TABLE divisao SET numero_janelas = 4 WHERE id_edificio = 4;
+UPDATE TABLE tarifa SET preco_dia = 0,3 WHERE id_tarifa = 1;
+UPDATE TABLE tarifa SET hora_mudanca_tarde = 13 WHERE id_tarifa = 1;
 
 /*DELETE*/
 DELETE edificio WHERE id_edificio = 1;
