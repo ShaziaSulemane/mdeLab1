9f13439244eca8dc9b65bfb48a6afce0cf3305ef
PedroMiguelAlbuquerque <pedroalbuquerque1998@gmail.com>
2019-04-18 18:35:00 +0100

    Procedures (create divisao and create equipamentoeletrico_divisao)

--- DML.sql
+++ DML.sql
@@ -1,16 +1,51 @@
 DROP PACKAGE package_dml;
-DROP PROCEDURE create_equip_div;
+DROP PROCEDURE create_div;
 
-CREATE OR REPLACE PROCEDURE create_equip_div IS
+/*CREATE SEQUENCE id_div
+ START WITH     1
+ INCREMENT BY   1
+ NOCACHE
+ NOCYCLE; */
+
+DROP PROCEDURE create_div;
+         
+CREATE OR REPLACE PROCEDURE create_div IS
        i INTEGER := 1;
+       j INTEGER :=1;
+       id_div INTEGER := 1;
     BEGIN
-        WHILE i <= 4
+        WHILE i <= 12
             LOOP
-                INSERT INTO equipamentoeletrico_divisao values (
-                    i, 1, 1, 
+            WHILE j <= 4
+                LOOP
+                INSERT INTO divisao values (id_div, i, j, 
+                    DBMS_RANDOM.value(low => 100, high => 500),
+                    DBMS_RANDOM.value(low => 3, high => 9), 
+                    DBMS_RANDOM.value(low => 1, high => 17));
+                id_div := id_div + 1;
+                END LOOP;
+            END LOOP; 
+            EXCEPTION
+    WHEN OTHERS THEN
+       raise_application_error(-20001,'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
+END;
+ 
+ CREATE OR REPLACE PROCEDURE create_equipele_div IS
+       i INTEGER := 1;
+       j INTEGER :=1;
+       equipele_div INTEGER := 1;
+    BEGIN
+        WHILE i <= 48
+            LOOP
+            WHILE j <= 10
+                LOOP
+                INSERT INTO dequipamentoeletrico_divisao values (equipele_div, i, j, 
+                    DBMS_RANDOM.value(low => 1, high => 16),
                     DBMS_RANDOM.value(low => 1, high => 9), 
                     DBMS_RANDOM.value(low => 1, high => 17));
-            END LOOP;
+                equipele_div:= equipele_div + 1;
+                END LOOP;
+            END LOOP;               
     EXCEPTION
     WHEN OTHERS THEN
        raise_application_error(-20001,'An error was encountered - '||SQLCODE||' -ERROR- '||SQLERRM);
